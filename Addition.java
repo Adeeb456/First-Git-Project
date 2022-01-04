771a2f0b14ef59c73e7bc7be4022d9dde01290c0
Adeeb Khan <adeebkhan4200@gmail.com>
2022-01-04 15:09:09 +0530

    Some Messages are added in print method for better understanding.

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ class Addition
         int a = new Scanner ( System.in ).nextInt () ;
         int b = new Scanner ( System.in ).nextInt () ;
 
-        System.out.println ( a+b ) ;
+        System.out.println ( "Addition of given numbers : "+a+b ) ;
     }
 }
