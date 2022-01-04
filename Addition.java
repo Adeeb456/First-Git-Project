664c724396749f9c8354bd9dbaa221d2a80c5959
Adeeb Khan <adeebkhan4200@gmail.com>
2022-01-04 12:38:36 +0530

    First Java Program File Commit !

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+    public static void main ( String [] args )
+    {
+        int a = new Scanner ( System.in ).nextInt () ;
+        int b = new Scanner ( System.in ).nextInt () ;
+
+        System.out.println ( a+b ) ;
+    }
+}
