047a7641e0200537d695797a40d57e30b8e5c65c
Adeeb Khan <adeebkhan4200@gmail.com>
2022-01-04 16:52:20 +0530

    Added some thankyou note at the end of program.

--- Addition.java
+++ Addition.java
@@ -1,10 +1,16 @@
+
+import java.util.*;
 class Addition
 {
     public static void main ( String [] args )
     {
-        int a = new Scanner ( System.in ).nextInt () ;
-        int b = new Scanner ( System.in ).nextInt () ;
+        Scanner sc = new Scanner ( System.in ) ;
+
+        int a = sc.nextInt () ;
+        int b = sc.nextInt () ;
+        sc.close () ;
 
         System.out.println ( "Addition of given numbers : "+a+b ) ;
+        System.out.println ( "Thanks for using this code." ) ;
     }
 }
